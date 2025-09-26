7f54e82bcd46d0b24ba5d260de636a2c08a322ba
Ido Motavasel <idomotavasel1234@gmail.com>
2025-09-26 14:44:44 +0300

    semi-fixed login form

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import styles from "./Login.module.css";
+import styles from "./login.module.css";
 import Form from "./components/form";
 
 const Login: React.FC = () => {
   return (
-    <div className="container">
-      <div className={styles.page}>
-        <div className={styles.formWrapper}>
-          <Form />
-        </div>
+    <div className={styles.page}>
+      <div className={styles.formWrapper}>
+        <Form />
       </div>
     </div>
   );
--- src/pages/login/components/form.tsx
+++ src/pages/login/components/form.tsx
@@ -7,41 +7,40 @@ const Form: React.FC = () => {
   const [password, setPassword] = useState("");
 
   return (
-      <div className={styles.card}>
-        <div className={styles.card2}>
-          <form className={styles.form}>
-            <p className={styles.heading}>Login</p>
+    <div className={styles.form3d}>
+      <div className={styles.formBack} />
+      <form className={styles.form}>
+        <p className={styles.heading}>התחברות </p>
 
-            <InputField
-              label="Username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              labelBgColor="#171717"
-              textBorderColor="#00ff75"
-              width="100%"
-              height="3rem"
-              textSize="1rem"
-            />
+        <InputField
+          label="מס' אישי"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          labelBgColor="#171717"
+          textBorderColor="#00ff75"
+          width="100%"
+          height="3rem"
+          textSize="1rem"
+        />
 
-            <InputField
-              label="Password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              labelBgColor="#171717"
-              textBorderColor="#3700ff"
-              width="100%"
-              height="3rem"
-              textSize="1rem"
-            />
+        <InputField
+          label="סיסמה"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          labelBgColor="#171717"
+          textBorderColor="#00ff75"
+          width="100%"
+          height="3rem"
+          textSize="1rem"
+        />
 
-            <div className={styles.btn}>
-              <button className={styles.button1}>Login</button>
-            </div>
-          </form>
+        <div className={styles.btn}>
+          <button className={styles.button1}>Login</button>
         </div>
-      </div>
+      </form>
+    </div>
   );
 };
 
