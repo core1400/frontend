21e9637a1d9ad3cb519d3e3f757640071f4745ad
Or Mell <oranus541@gmail.com>
2025-09-27 17:32:29 +0300

    chore: deleted FormTypes file

--- src/pages/login/types/FormTypes.ts
+++
@@ -1,9 +0,0 @@
-export interface ILoginFormValues {
-  personalNumber: string; 
-  password: string;       
-};
-
-export interface ILoginFormProps {
-  onSubmit?: (values: ILoginFormValues) => void | Promise<void>;
-  isLoading?: boolean;
-};
