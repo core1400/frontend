3e7cc75fde4990c88980899147624d1bfbd2777f
Or Mell <oranus541@gmail.com>
2025-09-27 21:19:08 +0300

    redux init + custom redux hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+import { store, persistor } from './store/store';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
