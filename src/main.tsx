08f6821f194a7fdf0a0277077fc79e688beafbba
Ido Motavasel <idomotavasel1234@gmail.com>
2025-10-01 16:15:30 +0300

    init routes with placeholders + fixed navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
